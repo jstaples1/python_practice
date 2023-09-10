04c6e2e99b92488e5c23b882497f39767d00c2ed
jstaples <jasonwstaples@gmail.com>
2023-09-10 10:28:37 -0700

    adding hangman

---
+++ general_practice/guessing_game.py
@@ -0,0 +1,26 @@
+
+
+def getCorrectAnswerWithRecursion(number):
+
+    answer = input('enter a number: ')
+    if number == int(answer):
+            print('Correct you win!')
+            return
+    else:
+         getCorrectAnswerWithRecursion(number)
+
+
+
+
+def getCorrectAnswerWithWhileLoop(number):
+    number = 5
+    print('Guess a number between 1 and 10.  You have unlimited guesses.')
+    
+    while True:
+
+        answer = input('enter a number: ')
+        print(answer)
+
+        if number == int(answer):
+            print('Correct you win!')
+            return
--- general_practice/hangman/hangman.py
+++ general_practice/hangman/hangman.py
@@ -1,34 +1,62 @@
+import sys
+import os
+os.system('clear')
+
+answer = 'basil'
+
 def main():
+    #print('Welcome to hangman')
+    guesses = 0
+    max_guesses= 5
+    print_hangman(0)
+    while guesses < max_guesses:
+        print('')
+        print('Please guess the name of the biggest asshole at THG, you have ' + str(max_guesses - guesses) + ' guesses remaining ')
+        guess = input(':')
+        if guess == answer:
+            guesses = max_guesses
+            print('')
+            print('Correct, '+ answer + ' is an asshole. You Win!')
+            sys.exit(0)
+        else:
+            guesses = guesses + 1  
+            print_hangman(guesses)
+
+    print('')
+    print('Too Many Guesses, You Lose!')          
     
-    getCorrectAnswerWithRecursion(5)
-    getCorrectAnswerWithWhileLoop(5)
 
-
-def getCorrectAnswerWithRecursion(number):
-
-    answer = input('enter a number: ')
-    if number == int(answer):
-            print('Correct you win!')
-            return
-    else:
-         getCorrectAnswerWithRecursion(number)
-
-
-
-
-def getCorrectAnswerWithWhileLoop(number):
-    number = 5
-    print('Guess a number between 1 and 10.  You have unlimited guesses.')
+def print_hangman(number):
     
-    while True:
+    body_parts = build_body_parts(number)
+    
+    os.system('clear')
+    print('      ')   
+    print('      ')
+    print('_____') 
+    print('|    |')
+    print('|',body_parts[0])
+    print('|',body_parts[1] + body_parts[2])
+    print('|',body_parts[3])
+    print('|',body_parts[4])
+    print('|'),
+    print('|______')
+        
+    return
 
-        answer = input('enter a number: ')
-        print(answer)
+def build_body_parts(number):
+    body_parts = ["   O","  /-","\ ","   |","  / \ "] 
+    parts = []
+    i = 0
+        
+    while i < len(body_parts):  
+        if i < int(number): 
+            parts.append(body_parts[i])
+        else:
+            parts.append("")
+        i = i + 1
+                
+    return parts     
 
-        if number == int(answer):
-            print('Correct you win!')
-            return
-            
-
-         
+                 
 main()    
